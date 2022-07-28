354540b91b57de4dd8ab5cf2aec3ebfbc79bd990
harshkacha1103 <99968991+harshkacha1103@users.noreply.github.com>
2022-07-28 13:16:17 +0530

    Add files via upload

---
+++ Exercise_27.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Exercise29
+{
+    public static void Main()
+    {
+        FileInfo f = new FileInfo("D:\\Demo.txt");
+        Console.WriteLine("\nSize of a file: " + f.Length.ToString());
+    }
+}
